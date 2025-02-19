cfa2f57ad0111c7dfd36cbfb61b0191ace386229
asa129 <asa.jad1290@gmail.com>
2025-02-19 17:50:12 +0900

    feat:DB登録機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { getAllDatas } from '../utils/supabaseFunctions';
+import { getAllDatas, insertData } from '../utils/supabaseFunctions';
 
 function App() {
   const [records, setRecords] = useState([]);
@@ -19,7 +19,11 @@ function App() {
       return setError(<p>入力されていない項目があります</p>);
     }
     setError("");
-    const newRecords = [...records, {title:studyContent, time:studyTime}] 
+    // データベースに記録する
+    const newData = {title:studyContent, time:studyTime};
+    insertData(newData);
+    // 表示用に配列に登録値を追加
+    const newRecords = [...records, newData] 
     setRecords(newRecords);
     // 合計時間の計算
     const time = newRecords.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.time), 0);
@@ -59,7 +63,7 @@ function App() {
       <div>入力されている学習内容:{studyContent}</div>
       <div>入力されている時間:{studyTime}</div>
       <ul>
-        {records.map((record) =>  <li>{`${record.title} ${record.time}時間`}</li>)}
+      {records.map((record) =>  <li>{`${record.title} ${record.time}時間`}</li>)}
       </ul>
       <button onClick={onClickAdd}>登録</button>
       {error}<br />
--- utils/supabaseFunctions.js
+++ utils/supabaseFunctions.js
@@ -4,3 +4,9 @@ export const getAllDatas = async() => {
   const datas = await supabase.from("study-record").select("*"); 
   return datas.data;
 };
+
+export const insertData = async(props) => {
+  const { title, time } = props;
+  const { error } = await supabase.from("study-record").insert({ title: title, time: time });
+  console.log(error);
+};
