36498c3f69f671c6e18403e2f1922f0716bf7ea5
asa129 <asa.jad1290@gmail.com>
2025-03-04 08:39:50 +0900

    test: 登録テスト修正

--- src/tests/regist.spec.js
+++ src/tests/regist.spec.js
@@ -1,7 +1,7 @@
 import App from "../App";
 import React from "react";
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent, within} from "@testing-library/react";
+import { render, screen, within, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("Regist Test", () => {
@@ -9,26 +9,25 @@ describe("Regist Test", () => {
 		// testId(title)を指定して取得
 		render(<App />);
 
-		// 学習内容と時間を設定する
-		const content = screen.getByTestId("studyContent");
-		const time = screen.getByTestId("studyTime");
-		fireEvent.change(content, {
-			target: { value: '学習内容' },
-		});
-		fireEvent.change(time, {
-			target: { value: 10 },
-		});
+		// 学習内容を設定する
+		await userEvent.type(screen.getByTestId("studyContent"), '学習内容');
+		// 時間を設定する
+		await userEvent.click(screen.getByTestId("studyTime"));
+		await userEvent.keyboard('10');
 
-		// 登録ボタンイベント実行
+		// 登録ボタン押下
 		const registButton = screen.getByTestId("add");
-
 		await userEvent.click(registButton);
+
+		テーブルの行を取得できるまで待つ
+		await waitFor(() => {
+			screen.getAllByRole("row");
+		});
 
 		// 一番下の要素を取得
 		const rows = screen.getAllByRole("row");
 		const lastRow = rows[rows.length - 1];
 		const lastRowCells = within(lastRow).getAllByRole("cell");
-
 
 		expect(lastRowCells[0].textContent).toBe("学習内容 10時間");
 
