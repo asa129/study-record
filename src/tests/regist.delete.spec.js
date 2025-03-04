9395806e4dd12e298187dfaf2ee6f7222fd4ec1a
asa129 <asa.jad1290@gmail.com>
2025-03-04 22:26:40 +0900

    test: 削除機能テスト追加, ファイル名変更

--- src/tests/regist.delete.spec.js
+++ src/tests/regist.delete.spec.js
@@ -6,7 +6,7 @@ import userEvent from "@testing-library/user-event";
 
 describe("Regist Test", () => {
 	it("フォームに入力、登録ボタンを押すと記録が追加されている数が1つ増えていること", async () => {
-		// testId(title)を指定して取得
+
 		render(<App />);
 
 		// 学習内容を設定する
@@ -31,5 +31,28 @@ describe("Regist Test", () => {
 
 		expect(lastRowCells[0].textContent).toBe("学習内容 10時間");
 
+	});
+
+	it("削除ボタンを押すと学習記録が削除される数が1つ減っていること", async () => {
+		
+		render(<App />);
+
+		// テーブルの行を取得できるまで待つ
+		await waitFor(() => {
+			screen.getAllByRole("row");
+		});
+
+		const before = screen.getAllByRole("row").length;
+
+		// テーブル最初の行の削除ボタン押下
+		const rows = screen.getAllByRole("row");
+		const firstRow = rows[0];
+		const deleteButton = within(firstRow).getByTestId("delete");
+		await userEvent.click(deleteButton);
+
+		// テーブルの行が1行削除されていること
+		await waitFor(() => {
+			expect(screen.getAllByRole("row").length).toBe(before - 1);
+		});
 	});
 });
